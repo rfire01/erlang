103f00a8a8295c4c5839d5fe7a9ac1c39302ef2b
rfire01 <rfire01@gmail.com>
2016-02-14 15:59:05 +0200

    one node gen_server added

--- unit_server.erl
+++ unit_server.erl
@@ -10,35 +10,29 @@
 -behaviour(gen_server).
 
 % interface calls
--export([start/1, stop/1]).
+-export([start/1]).
     
 % gen_server callbacks
--export([init/1,
-         handle_call/3, 
-         handle_cast/2,
-         handle_info/2, 
-         terminate/2, 
-         code_change/3]).
+-export([init/1,handle_call/3,handle_cast/2,
+         handle_info/2,terminate/2, code_change/3]).
+		 
+-define(SERVER, ?MODULE).
 
 %%====================================================================
 %% Server interface
 %%====================================================================
 %% Booting server (and linking to it)
-start(ServName) -> 
-    io:format("Starting with ~p~n",[1]),
-    gen_server:start_link({local, ServName}, ?MODULE, [1], []).
+start() -> 
+    gen_server:start({local, ?SERVER}, ?MODULE, [], []).
 
-%% Stopping server asynchronously
-stop(ServName) ->
-    io:format("Stopping~n"),
-    gen_server:cast(ServName, shutdown).
 
 %%====================================================================
 %% gen_server callbacks
 %%====================================================================
-init([Pars]) ->
-    io:format("Initializing with ~p~n",[Pars]),
-    process_flag(trap_exit, true),
+init([]) ->
+    io:format("starting local_gen: ~p~n",[?SERVER]),
+	ets:new(general_info,[set,named_table]),
+	ets:new(sen_fire,[set,named_table]),
     {ok, initialized}.
 
 %% Synchronous, possible return values  
@@ -60,18 +54,68 @@ handle_call(Message, From, State) ->
 % {noreply,NewState,hibernate}
 % {stop,Reason,NewState}
 %% normal termination clause
-handle_cast({unlock,Code}, State) ->
-	gen_fsm:sync_send_event(gfsm, {unlock, Code}),
-    io:format("Generic cast handler: '~p' while in '~p'~n",[{unlock,Code}, State]),
-    {noreply, State};
-handle_cast({lock,Code}, State) ->
-	gen_fsm:sync_send_event(gfsm, {lock, Code}),
-    io:format("Generic cast handler: '~p' while in '~p'~n",[{lock,Code}, State]),
-    {noreply, State};
+handle_cast({create,DataList}, State) ->
 
-handle_cast(shutdown, State) ->
-    io:format("Generic cast handler: *shutdown* while in '~p'~n",[State]),
-    {stop, normal, State};
+	io:format("stoping all old fsms ~n"),
+	ObjList = ets:tab2list(general_info),
+	[gen_fsm:stop({global,H}) || {{_,H},_,_,_} <- ObjList],
+	ets:delete_all_objects(general_info),
+
+	io:format("insert data: ~p~n",[DataList]),
+	ets:insert(general_info,DataList),
+	io:format("starting helicopters ~n"),
+	HeliList = ets:match(general_info,{{heli,'$1'},'$2','$3','_'}),
+	[heli:start([Name,X,Y]) || [Name,X,Y] <- HeliList],
+	io:format("starting fires ~n"),
+	FireList = ets:match(general_info,{{fire,'$1'},'$2','$3','$4'}),
+	[fire:start([Name,X,Y,R]) || [Name,X,Y,R] <- FireList],
+	io:format("starting sensors ~n"),
+	HeliList = ets:match(general_info,{{sensor,'$1'},'$2','$3','$4'}),
+	[sensor:start([Name,X,Y,R]) || [Name,X,Y,R] <- SenList],
+    {noreply, State};
+	
+handle_cast({update,Unit_Type,Unit_Data}, State) ->
+	case Unit_Type of
+		heli -> [Name,X,Y,Status]=Unit_Data,	io:format("updating helicopter: ~p~n",[Name]),
+				ets:insert(general_info,{{heli,Name},X,Y,Status});
+		fire -> [Name,XF,YF,RF]=Unit_Data,		io:format("updating fire: ~p~n",[Name]),
+				ets:insert(general_info,{{fire,Name},XF,YF,RF}),
+				
+				%%send alerts to all servers
+				QH = qlc:q([{SName,X,Y,R} || {{sensor,SName},XS,YS,RS} <- ets:table(general_info), ((XF-XS)*(XF-XS) + (YF-YS)*(YF-YS))< (RS+RF)*(RS+RF)]),
+
+				case qlc:eval(QH) == [] of
+					true -> dont_care;
+					false -> send_alerts(SensorList) %% TODO
+				end
+	end,
+	{noreply, State};	
+	
+handle_cast({heli_request,Sen_name,Fire_Name}, State) ->
+	
+	io:format("requesting heli ~n"),
+	Exists = ets:member(sen_fire,{Sen_name,Fire_Name}),
+	case Exists of
+		true -> do_nothing, io:format("heli already sent ~n"),;
+		false ->  QH = qlc:q([{{HName,HX,HY} || {{heli,HName},HX,HY,not_working} <- ets:table(general_info)]),
+
+				   case qlc:eval(QH)  of
+						[]-> wait_for_free_heli;
+						[{Name,X,Y}|_] -> ets:insert(sen_fire,{{Sen_name,Fire_Name},true}),
+										  ets:insert(general_info,{{heli,Name},X,Y,working}),
+										  [{{_,_},SX,SY,SR}] = ets:lookup(general_info,{sensor,Sen_name}),
+										  io:format("sending heli ~p~n",[Name]),
+										  heli:move_dst(SX+SR,SY,search_fire)
+				   end
+	end
+	
+	{noreply, State};	
+					
+handle_cast({heli_done,Name}, State) ->
+	io:format("helicopter: ~p is free ~n",[Name]),
+	ets:update_element(general_info,{heli,Name},{4,not_working}),
+	{noreply, State};
+
 %% generic async handler
 handle_cast(Message, State) ->
     io:format("Generic cast handler: '~p' while in '~p'~n",[Message, State]),
